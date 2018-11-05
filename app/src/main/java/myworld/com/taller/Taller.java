f8e0a3dc72086a6e55bf0b710436b2451bbd947f
MariBaque <mmbaque2018s1@gmail.com>
2018-11-05 10:52:07 -0500

    comite

---
+++ app/src/main/java/myworld/com/taller/Taller.java
@@ -0,0 +1,13 @@
+package myworld.com.taller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Taller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taller);
+    }
+}
